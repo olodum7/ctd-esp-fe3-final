c84cefc06c3f18c92fb74642f7275b4fb1d18632
Emanuel Espinel <olodum7@gmail.com>
2024-09-08 20:30:16 -0300

    Improve card styles

--- components/card/card.component.tsx
+++ components/card/card.component.tsx
@@ -8,38 +8,58 @@ import Typography from "@mui/material/Typography";
 import { Box } from "@mui/material";
 import { IComic } from "types/IComic.type";
 import NextLink from "next/link";
+import { styled } from "@mui/system";
+
+const StyledButton = styled(Button)({
+  backgroundColor: "black",
+  color: "white",
+  borderRadius: "20px",
+  marginLeft: "16px",
+  "&:hover": {
+    backgroundColor: "#333",
+  },
+});
+
+const StyledLink = styled("a")({
+  color: "black",
+  textDecoration: "underline",
+  marginLeft: "-6px",
+  cursor: "pointer",
+  "&:hover": {
+    textDecoration: "none",
+  },
+});
 
 interface Props {
-    comic: IComic;
+  comic: IComic;
 }
 
 const CardComponent: FC<Props> = ({ comic }) => {
-
-return (
-    <Card sx={{maxHeigth : 500}}>
-        <Box>
-            <CardMedia
-            component="img"
-            height="350"
-            image={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
-            alt={comic.title}
-            />
+  return (
+    <Card sx={{ maxHeigth: 500 }}>
+      <Box>
+        <CardMedia
+          component="img"
+          height="350"
+          image={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
+          alt={comic.title}
+        />
         <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-                {comic.title}
-            </Typography>
-        </CardContent>
-        </Box>
-        <CardActions>
+          <Typography gutterBottom variant="h5" component="div">
+            {comic.title}
+          </Typography>
+          <CardActions>
             <NextLink href={`/comics/${comic.id}`}>
-                <Button>Ver detalles</Button>
+              <StyledLink>Ver detalles</StyledLink>
             </NextLink>
-            <NextLink href={`/checkout?comic=${comic.id}`} >
-            COMPRAR
+            <NextLink href={`/checkout?comic=${comic.id}`}>
+              <StyledButton>COMPRAR</StyledButton>
             </NextLink>
-        </CardActions>
+          </CardActions>
+        </CardContent>
+      </Box>
     </Card>
-);
+  );
 };
 
 export default CardComponent;
